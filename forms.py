c054159fa52b4a93fdb3ce27037232b37d466037
kamal-khadka <38675025+kamal-khadka@users.noreply.github.com>
2020-08-04 08:26:51 -0700

    errors feedback when form submission

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, TextAreaField, SelectField
-from wtforms.validators import Required, Email
+from wtforms.validators import Required, Email, ValidationError
+import phonenumbers
 # from wtforms.widgets import TextArea
 
 
@@ -11,6 +12,14 @@ class SignupForm(FlaskForm):
     password = PasswordField('Password', validators=[Required('Password is required')])
     phone = StringField('Phone', validators=[Required()])
 
+    def validate_phone(self, phone):
+        try:
+            p = phonenumbers.parse('+1'+phone.data)
+            if not phonenumbers.is_valid_number(p):
+                raise ValueError()
+        except (phonenumbers.phonenumberutil.NumberParseException, ValueError):
+            raise ValidationError('Invalid phone number')
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[Required('Email is required'), Email()])
@@ -20,7 +29,7 @@ class ContactForm(FlaskForm):
     first_name = StringField('First name', validators=[Required()])
     last_name = StringField('Last name', validators=[Required()])
     # phone = StringField('Phone', validators=[Required()])
-    email = StringField('Email', validators=[Required()])
+    email = StringField('Email', validators=[Required(), Email()])
 
 class GroupForm(FlaskForm):
     name = StringField('Group name', validators=[Required()])
